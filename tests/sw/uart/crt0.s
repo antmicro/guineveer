2cae3f689b087e1c1d4ea3372bfc96213a0432b5
Filip Kokosinski <fkokosinski@antmicro.com>
2025-02-06 17:39:12 +0100

    Add UART test

---
+++ tests/sw/uart/crt0.s
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: Apache-2.0
+
+.section .text.init
+.global _start
+_start:
+        # enable caching starting from region 0x8
+        # put side effect in region 0x3
+        li t0, 0x00010090
+        csrw 0x7c0, t0
+        # Setup stack
+        la sp, __stack_start
+
+        # Call main()
+        call main
+
+        # Map exit code: == 0 - success, != 0 - failure
+        mv  a1, a0
+        li  a0, 0xff # ok
+        beq a1, x0, _finish
+        li  a0, 1 # fail
+
+.global _finish
+_finish:
+        la t0, __mailbox
+        sb a0, 0(t0) # Signal testbench termination
+        beq x0, x0, _finish
+        .rept 10
+        nop
+        .endr
+
+.section .data.mailbox
+.global __mailbox
+__mailbox:
+	.word 0
